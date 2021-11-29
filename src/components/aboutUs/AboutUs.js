1ad0bc74db892946c49b4019f5ee0f2cd9a6a563
rahatkabir007 <rahatkabir04@gmail.com>
2021-11-29 13:30:15 +0600

    about e aimer

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -17,7 +17,7 @@ const AboutUs = () => {
                     <img className="w-100 p-4" src={require("../../images/slider (4).jpg").default} alt="" srcset="" />
                 </Col>
             </Row>
-            <h4 className="text-center m-5 p-4">Established in 2008, Aimers empowers students in their pursuit of learning, discovery, leadership and engagement. Located in a sprawling metropolis, our premier Tier One campus provides students with cutting-edge programs including undergraduate, graduate, doctoral, distance and continuing education studies. Ranked among the best Institution in Bangladesh, UH is home to award-winning faculty, innovative research centers, alumni who have become international leaders and one of the most diverse student populations in the nation. At Aimers, we prepare students to envision their future, emerge as leaders and launch careers that transform the world.</h4>
+            <h4 className="text-center m-5 p-4">Aimers Established in 2008, Aimers empowers students in their pursuit of learning, discovery, leadership and engagement. Located in a sprawling metropolis, our premier Tier One campus provides students with cutting-edge programs including undergraduate, graduate, doctoral, distance and continuing education studies. Ranked among the best Institution in Bangladesh, UH is home to award-winning faculty, innovative research centers, alumni who have become international leaders and one of the most diverse student populations in the nation. At Aimers, we prepare students to envision their future, emerge as leaders and launch careers that transform the world.</h4>
         </div>
     );
 };
